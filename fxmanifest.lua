3c419b41065a69f8cfcde559cbed32e44967d21b
Barry Seal <thebarrymeister101@gmail.com>
2025-01-22 07:29:46 -0600

    Added fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Barry Seal'
+description 'Patrol Log Script'
+version '1.0.0'
+
+server_scripts {
+    'config/config.lua',
+    'server/server.lua'
+}
